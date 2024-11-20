7768089724246e7d0fd9edb671ab1c0f7c8cc77b
KwonHyeji <kwonhyeji007@gmail.com>
2024-11-20 10:55:21 +0900

    11/20 spring class

--- src/main/java/com/green/greengramver1/user/model/UserInsReq.java
+++ src/main/java/com/green/greengramver1/user/model/UserInsReq.java
@@ -1,5 +1,6 @@
 package com.green.greengramver1.user.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.ToString;
@@ -8,8 +9,14 @@ import lombok.ToString;
 @AllArgsConstructor
 @ToString
 public class UserInsReq {
+    @Schema(description = "유저 아이디", example = "mic"
+            , requiredMode = Schema.RequiredMode.REQUIRED)
     private String uid;
+    @Schema(description = "유저 비밀번호", example = "1212"
+            , requiredMode = Schema.RequiredMode.REQUIRED)
     private String upw;
+    @Schema(description = "유저 프로필 사진")
     private String pic;
+    @Schema(description = "유저 닉네임", example = "홍길동")
     private String nickName;
 }
